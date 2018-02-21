e9a43e5ea2722056a459114054b3f84da9f2f510
Fadi A <royalfadich@gmail.com>
2018-02-21 23:07:31 +0200

    ``` Created

--- analytics/urls.py
+++ analytics/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.info, name='info')
+    url(r'^$', views.info, name='info'),
+    url(r'^api/(?P<region>\w+)/(?P<account>\w+)/$', views.api_info, name='api-info'),
 ]
