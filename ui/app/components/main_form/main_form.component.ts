a238a87b38a1e72831c8f71d8ee5516a8b0ce8e5
Waseem9287 <wahmad@cloudlinux.com>
2018-05-04 17:31:21 +0300

    UI added form

---
+++ ui/app/components/main_form/main_form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'main-form',
+    templateUrl: 'components/main_form/main_form.component.html',
+    styleUrls: ['components/main_form/main_form.component.scss'],
+})
+
+export class MainFormComponent {
+
+}
--- ui/app/core/app.module.ts
+++ ui/app/core/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from '../components/app/app.component';
+import { MainFormComponent } from '../components/main_form/main_form.component';
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent],
+    declarations: [AppComponent, MainFormComponent],
     bootstrap: [AppComponent],
 })
 
